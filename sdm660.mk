09d8ada5c75ac52973e7d38b87dccab486dd71e2
SakilMondal <mondalsakil@gmail.com>
2020-12-11 21:52:53 +0530

    sdm660-common: Build a2dp and add it to manifest
    I/hwservicemanager(575): getTransport: Cannot find entry android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default in either framework or device manifest.
    E/LegacySupport(24035): Could not get passthrough implementation for android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default.
    
    Change-Id: Ifd1c2c16cf213abb8ef4415af4fa7012ab7d7bf3
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -92,7 +92,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
@@ -127,6 +126,12 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+
+# A2DP
+PRODUCT_PACKAGES += \
+  audio.a2dp.default \
+  android.hardware.bluetooth.a2dp@1.0-impl \
+  android.hardware.bluetooth.a2dp@1.0-service
 
 # ANT+
 PRODUCT_PACKAGES += \
