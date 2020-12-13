acc39696ed0ed1ce71eefd9006eb6639423df041
sbwml <sagit.sbwml.net>
2020-12-13 12:30:28 +0000

    sdm660-common: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so
    
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -214,9 +214,14 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.1_vendor
+    android.frameworks.displayservice@1.0 
+
+
+
+PRODUCT_PACKAGES += \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
+
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -502,7 +507,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libaacwrapper \
+    libaacwrapper
     libnl
 
 PRODUCT_BOOT_JARS += \
